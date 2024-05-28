5acc23478e4c156c68f4c931604aee9daa9ea879
Erik Král <ekral@utb.cz>
2024-05-28 08:05:47 +0200

    View number of people online

--- ServerManagement/Models/Server.cs
+++ ServerManagement/Models/Server.cs
@@ -13,10 +13,14 @@ namespace ServerManagement.Models
         [Required]
         required public string City { get; set; }
 
+        public int NumberOfPeople { get; set; }
+
         public Server()
         {
             int randomNumber = Random.Shared.Next(0, 2);
             IsOnline = randomNumber == 0 ? false : true;
+
+            NumberOfPeople = Random.Shared.Next(0, 100);
         }
     }
 }
