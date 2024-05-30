827505a59c5e02f22f3e5b7cb6bc86a0717cf494
Erik Král <ekral@utb.cz>
2024-05-30 13:55:17 +0200

    add events log

--- ServerManagement/CitiesRepository.cs
+++ ServerManagement/CitiesRepository.cs
@@ -2,7 +2,7 @@
 {
     public class CitiesRepository : ICitiesRepository
     {
-        private readonly List<string> cities = ["Toronto", "Montreal", "Ottawa", "Calgary", "Halifax"];
+        private readonly List<string> cities = ["Toronto"/*, "Montreal", "Ottawa", "Calgary", "Halifax"*/];
 
         public List<string> GetCities() => cities;
     }
